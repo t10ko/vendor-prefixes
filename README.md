faa04045ea3c3b16dc07144d471c4e4e2c58a64f
Tigran Atayan <tiko.atayan@gmail.com>
2016-10-05 22:41:26 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# JavaScript API for checking vendor prefix versions.
+
+[Vendor prefixes](https://developer.mozilla.org/en-US/docs/Glossary/Vendor_Prefix) are used by browsers to make non standart features.
+
+But they only make problems.  
+Here is a short API to handle vendor prefixes.
+
+# API reference.
+
+### VendorPrefixes.versions( value, type )
+
+**versions** make and return vendor prefixed versions of a **value**.
+Type is vendor prefixing type.  
+Can be **'JS'**, **'JSClass'**, **'CSS'**, , **'const'**, **'event'**.
+
+**JS** is for javascript functions that can have prefixes.
+**JSClass** is for javascript classes.
+**CSS** is for css properties.
+**const** is for javascript class constants.
+**event** is for HTML event names.
+
+Here are examples.
+```javascript
+console.log( VendorPrefixes.versions( 'requestAnimationFrame', 'JS' ) );
+console.log( VendorPrefixes.versions( 'MutationObserver', 'JSClass' ) );
+console.log( VendorPrefixes.versions( 'transition', 'CSS' ) );
+console.log( VendorPrefixes.versions( 'KEYFRAMES_RULE', 'const' ) );
+console.log( VendorPrefixes.versions( 'transitionStart', 'event' ) );
+```
+
+the only difference of type **event** is that microsofts prefix is other then standart JS functions prefixes.
+
+### VendorPrefixes.try( value, type, helper )
+
+This tries to find a prefix of current system.  
+This tries all versions of **type** type of **value** trying to find out which one is supported.  
+**helpier** is that helper function which is called for every version and helps this API to find out supports.  
+Helper must return a boolean value, which indicates is that version supported or not.
+
+**try** returns object( with version and prefix values ) if a vendor prefixed version of **value** matched, or null.
+
+If vendor prefix version for current system is already found, this will try only *that* vendor prefixed version of **value**.
+
+Examples.
+```javascript
+//  Try this using webkit.
+VendorPrefixes.try( 'requestAnimationFrame', 'JS', function ( version ) {
+    return !!window[ version ];
+} );
+VendorPrefixes.try( 'MutationObserver', 'JSClass', function ( version ) {
+    return !!window[ version ];
+} );
+```
+
+### VendorPrefixes.make( value )
+
+If vendor prefix is found for current system, this will prefix given **value** with that prefix.
+```javascript
+//  Again, try this using webkit.
+VendorPrefixes.try( 'requestAnimationFrame', 'JS', function ( version ) {
+    return !!window[ version ];
+} );
+
+console.log( VendorPrefixes.make( 'cancelAnimationFrame' ) );
+```
+
+If no vendor prefix is found, this will return false.
+```javascript
+//  Try this without calling 'try' method before it.
+console.log( VendorPrefixes.make( 'requestAnimationFrame' ) );
+```
+
+### VendorPrefixes.matched( version )
+
+If you tried something on your own and found vendor prefix for current system, give it to this function.
+This will save that prefix **version** as current system's default.
+
+```javascript
+//  Will print false.
+console.log( VendorPrefixes.make( 'requestAnimationFrame' ) );
+
+if( window.webkitCancelAnimationFrame )
+    VendorPrefixes.matched( 'webkit' );
+
+//  Will print webkitRequestAnimationFrame.
+console.log( VendorPrefixes.make( 'requestAnimationFrame' ) );
+```
+
+## Browser support
+
+I haven't used any special functionality of JS, so this is fully cross browser.
---
+++ main.js
@@ -0,0 +1,160 @@
+( function ( scope, factory ) {
+	if( typeof exports === 'object' && typeof module === 'object' ) {
+		module.exports = factory( scope );
+	} else if( typeof define === 'function' && define.amd ) {
+		define( [], function () { return factory( scope ); } );
+	} else {
+		( typeof exports === 'object' ? exports : window ).VendorPrefixes = factory( scope );
+	}
+} ) ( this, function () {
+	function ShiftObject( target ) {
+		for( var key in target ) 
+			return { key: key, value: PopProp( target, key ) };
+	};
+	function PopProp( target, property ) {
+		var value = target[ property ];
+		delete target[ property ];
+		return value;
+	}
+	var HasOwn = ( function () {
+		var has_own = {}.hasOwnProperty;
+		return function HasOwn( target, name ) {
+			return !!name && has_own.call( target, name );
+		};
+	} ) ();
+	var self = {}, 
+		main = self, 
+		prefix_to_index = {}, 
+		prefixes = [], 
+		default_type = 'JS', 
+		matched = false;
+
+	( function () {
+		var list = [ 'Webkit', 'Moz', 'MS', 'O' ], i = 0;
+		for( ; i < list.length; i++ ) {
+			var pre = list[i], 
+				lower = pre.toLowerCase(), 
+				for_css = '-' + lower + '-', 
+				upper = pre.toUpperCase() + '_', 
+				for_event = pre == 'MS' ? pre : lower;
+
+			prefixes.push( { JS: lower, JSClass: pre, CSS: for_css, const:  upper, event: for_event } );
+			prefix_to_index[ lower ] = i;
+		}
+	} ) ();
+
+	function Match( index ) {
+		var info = prefixes[ index ];
+		matched = true;
+		prefixes = [ info ];
+		main.current = info[ 'JS' ];
+	};
+	function FirstToUpper( value ) { return value[0].toUpperCase() + value.slice(1); };
+	function PrefixToJS( value ) { return value.toLowerCase().replace( /^\-|\-$/g, '' ); };
+	function CheckMatched() {
+		if( !matched )
+			throw new Error( 'Vendor prefix of this browser is not found yet.' );	
+		return matched;
+	};
+	function GetVersions( value, type ) {
+		if( !main.checkType( type ) )
+			type = default_type;
+
+		var is_event = type == 'event', 
+			result = {}, 
+			i = 0;
+
+		if( is_event || type == 'JS' || type == 'JSClass' ) 
+			value = FirstToUpper( value );
+
+		for( ; i < prefixes.length; i++ ) {
+			var info = prefixes[i];
+			result[ info[ 'JS' ] ] = info[ type ] + value;
+		}
+		return result;
+	};
+
+	/**
+	 * Currently fixed prefix.
+	 * @type {String}
+	 */
+	main.current = null;
+
+	/**
+	 * Checks if given type is valid.
+	 * @param  {String}		type
+	 * @return {Boolean}
+	 */
+	main.checkType = function ( type ) { return HasOwn( prefixes[0], type ); };
+
+	/**
+	 * Get all possible verions for a string.
+	 * If vendor already choosen, 
+	 * it will not return only not prefixed and that vendor prefixed versions.
+	 * @param  {String}		value		String to prefix.
+	 * @param  {UINT}		type		Prefix type to use.
+	 * @param  {Boolean}	as_array	Return results as array, or as object which keys are prefixes and values are prefixed versions.
+	 * @return {Mixed}		
+	 */
+	main.versions = function ( value, type, as_array ) {
+		var result = GetVersions( value, type );
+		return as_array ? ObjectValues( result ) : result;
+	};
+
+	/**
+	 * Try all versions for specified item.
+	 * This loops over all verions and executes given checker function on all of the versions.
+	 * Checker must return true on success.
+	 * @param	{String}		value	
+	 * @param	{UINT}		type	
+	 * @param	{Function}	checker	
+	 * @return	{Object}				null if nothing worked, working prefix version information otherwise.
+	 */
+	main.try = function ( value, type, checker ) {
+		var versions = GetVersions( value, type ), 
+			i = 0;
+		for( var prefix in versions ) {
+			var current = versions[ prefix ];
+
+			//	If this version is supported.
+			//	Save this as the main version, and return it.
+			if( checker( current ) ) {
+				if( !matched )
+					Match( i );
+
+				//	Filling prefix to value map in result.
+				return {
+					prefix: prefix, 
+					version: current
+				};
+			}
+			i++;
+		}
+		return null;
+	};
+
+	/**
+	 * Make a vendor prefix version for given value.
+	 * @param	{String}	value	Value that needs to be prefixed.
+	 * @param	{UINT}		type 	Prefix type.
+	 * @return	{String} 		
+	 */
+	main.make = function ( value, type ) {
+		return CheckMatched() && ShiftObject( GetVersions( value, type ) ).value;
+	};
+
+	/**
+	 * Implicitly set a vendor.
+	 * @param	{String}	prefix	Prefix string.
+	 */
+	main.matched = function ( prefix ) {
+		if( matched ) 
+			return false;
+		prefix = PrefixToJS( prefix );
+		var choosed = HasOwn( prefix_to_index, prefix );
+		if( choosed )
+			Match( prefix_to_index[ prefix ] );
+		return choosed;
+	};
+	return main;
+} );
